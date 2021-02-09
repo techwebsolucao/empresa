9ac43411562a2d6980c85de96e359a53273cd200
eduardoterrors <eduardoterrors@gmail.com>
2021-02-09 16:18:30 -0300

    ADD produto GET/POST

--- resources/views/sistema/Produto.blade.php
+++ resources/views/sistema/Produto.blade.php
@@ -44,33 +44,25 @@
             <!-- Begin Page Content -->
             <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                    <a href="{{route('cadastrar_produto')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                            class="fas fa-download fa-sm text-white-50"></i> Cadastrar produto</a>
-                </div>
-
                 <!-- Content Row -->
-                <div class="row">
+                <div class="row col-10">
+                    <form action="{{route('escrever_produto')}}" method="post">
+                        <div class="card p-3">
+                            <div class="col-sm">
+                                @csrf
+                                <p class="m-1">Nome do produto:</p>
+                                <input class="form-control" required="required" type="text" name="nome_produto">
 
-                    <!-- Pending Requests Card Example -->
-                    <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-warning shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                            Pending Requests</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
+                                <p class="m-1">Quantidade:</p>
+                                <input class="form-control" required="required" type="text" name="quantidade">
+
+                                <p class="m-1">Codigo do produto</p>
+                                <input class="form-control" required="required" type="text" name="codigo_produto">
+
+                                <button type="submit" class="btn btn-sm btn-success mt-5">Cadastrar Produto</button>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
 
             </div>
