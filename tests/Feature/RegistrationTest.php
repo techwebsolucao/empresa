cd30144f3fbbc701dbcbe571ada92a57ba75bde7
eduardoterrors <eduardoterrors@gmail.com>
2021-02-09 20:34:04 -0300

    Update RegistrationTest.php

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -24,8 +24,7 @@ class RegistrationTest extends TestCase
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
-            'password_confirmation' => 'password',
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+            'password_confirmation' => 'password'
         ]);
 
         $this->assertAuthenticated();
