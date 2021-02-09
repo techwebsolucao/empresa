a1f5b0daa50a87e42198ffada347739289eb471d
eduardoterrors <eduardoterrors@gmail.com>
2021-02-09 20:51:13 -0300

    Update EntrarController.php

--- app/Http/Controllers/EntrarController.php
+++ app/Http/Controllers/EntrarController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EntrarController extends Controller
 {
     public function index(){
         return view('dashboard');
+    }
+
+    public function logout(){
+        if(Auth::check()){
+            Auth::logout();
+            return redirect()->route('login');
+        }
+        return redirect()->route('login');
     }
 }
