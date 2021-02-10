ec40e39c155386dfd5d8181899361346f8f54114
eduardoterrors <eduardoterrors@gmail.com>
2021-02-09 21:49:18 -0300

    Update EntrarController.php
    Criando a funcao Login para API

--- app/Http/Controllers/EntrarController.php
+++ app/Http/Controllers/EntrarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class EntrarController extends Controller
 {
@@ -17,5 +18,21 @@ class EntrarController extends Controller
             return redirect()->route('login');
         }
         return redirect()->route('login');
+    }
+
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|'
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['status_code' => 400, 'mensagem' => 'Bad Request']);
+        }
+
+        $crede = request(['email', 'password']);
+        if(!Auth::attempt($crede)){
+            return response()->json(['status_code' => 500, 'mensagem' => 'Não autorizado']);
+        }
     }
 }
