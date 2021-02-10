4ae983bf374efb649afb9f91ef8a549e5340b671
eduardoterrors <eduardoterrors@gmail.com>
2021-02-09 23:39:20 -0300

    Update navbar.blade.php

--- resources/views/sistema/partes/navbar.blade.php
+++ resources/views/sistema/partes/navbar.blade.php
@@ -1,6 +1,9 @@
 
 @php
     use Illuminate\Support\Facades\DB;
+    use Illuminate\Support\Facades\Auth;
+
+    $nome = Auth::user()->name;
     $coletarDadosQuantidade = DB::table('produtos')->where('quantidade', '<=', '100')->get();
     $coletarDadosQuantidadeCount = DB::table('produtos')->where('quantidade', '<=', '100')->count();
 @endphp
@@ -68,17 +71,16 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">@php echo "Nome" @endphp</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{$nome}}</span>
                 <img class="img-profile rounded-circle"
-                     src="img/undraw_profile.svg">
+                     src="{{asset('dashboardTemplate/img/undraw_profile.svg')}}">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                  aria-labelledby="userDropdown">
-                <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
+                    Sair
                 </a>
             </div>
         </li>
