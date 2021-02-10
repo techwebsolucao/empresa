3683ecc5e0cd4d70d1603f65c632c92d2c90589c
eduardoterrors <eduardoterrors@gmail.com>
2021-02-10 17:33:36 -0300

    Update RelatorioController.php
    Adicionado a forma de buscar via post

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
 
 class RelatorioController extends Controller
 {
-    //
+    public function __construct()
+    {
+        Paginator::useBootstrap();
+    }
+
+    public function buscarBaixa(Request $request){
+        $dados = $request->all();
+        $relatorio = DB::table('relatorios')
+           ->where('opcao', '=', $dados['opcao'])
+           ->where('data', '=', $dados['data'])
+           ->get();
+        $relatorioCount = DB::table('relatorios')
+            ->where('opcao', '=', $dados['opcao'])
+            ->where('data', '=', $dados['data'])
+            ->count();
+
+        return view('sistema.relatorioVi', ['relatorio' => $relatorio, 'relatorioCount' => $relatorioCount]);
+    }
 }
