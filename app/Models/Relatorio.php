55f8cb56442a3525d9c3b10b182fc86b4c90db9a
eduardoterrors <eduardoterrors@gmail.com>
2021-02-10 19:00:45 -0300

    update Relatorios to Relatorio

--- app/Models/Relatorio.php
+++ app/Models/Relatorio.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Relatorios extends Model
+class Relatorio extends Model
 {
     use HasFactory;
 
