22e5cc6d8ee696958abc5145e32ca197ee54dd9a
eduardoterrors <eduardoterrors@gmail.com>
2021-02-09 21:48:45 -0300

    Update api.php
    Adicionado rota login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\EntrarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/login', [EntrarController::class, 'login'])->name('login'); #ok
