ec816d19093e5b4fc2106389a9f0e4dfaa47296b
eduardoterrors <eduardoterrors@gmail.com>
2021-02-09 23:37:21 -0300

    Update ProdutosController.php

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -38,6 +38,7 @@ class ProdutosController extends Controller
         return redirect()->route('cadastrar_produto');
     }
 
+
     public function removerProduto(Request $request){
         $dados = $request->all();
         Produtos::destroy($dados['id']);
@@ -69,17 +70,51 @@ class ProdutosController extends Controller
 
     public function baixarProdutosEscrever(Request $request){
         $dados = $request->all();
-        $coletar = DB::table('produtos')->whereIn('id', $dados['produtos'])->where('quantidade', '>=', $dados['quantidade'])->get();
         try {
+            $coletar = DB::table('produtos')->whereIn('id', $dados['produtos'])->where('quantidade', '>=', $dados['quantidade'])->get();
+
             foreach($coletar as $tabela){
                 DB::table('produtos')
                     ->where('id', '=', $tabela->id)
                     ->update(['quantidade' => $tabela->quantidade -= $dados['quantidade']]);
             }
-
             return redirect()->route('baixar_produtos');
         }catch (\Exception $exception){
             return redirect()->route('baixar_produtos');
+        }
+
+    }
+
+    //API
+    public function adicionarProdutoApi(Request $request){
+        $dados = $request->all();
+
+        try{
+            Produtos::create([
+                'codigo_produto' => $dados['codigo_produto'],
+                'nome' => $dados['nome'],
+                'quantidade' => $dados['quantidade'],
+                'data' => date('Y-m-d')
+            ]);
+            return response()->json(['status' => 200, 'mensagem' => 'Produto criado com sucesso']);
+
+        }catch (\Exception $e){
+            return response()->json(['status' => 500, 'mensagem' => $e->getMessage()]);
+        }
+    }
+
+    public function baixarProdutoApi(Request $request){
+        $dados = $request->all();
+        try {
+            $coletar = DB::table('produtos')->whereIn('id', $dados['produtos'])->where('quantidade', '>=', $dados['quantidade'])->get();
+            foreach($coletar as $tabela){
+                DB::table('produtos')
+                    ->where('id', '=', $tabela->id)
+                    ->update(['quantidade' => $tabela->quantidade -= $dados['quantidade']]);
+            }
+            return response()->json(['status' => 200, 'mensagem' => 'Produto criado com sucesso']);
+        }catch (\Exception $exception){
+            return response()->json(['status' => 500, 'mensagem' => 'Algo não está correto no post']);
         }
 
     }
